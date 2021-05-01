b7f02c3d7eec2c72eab895c5021c6445d31753a1
YoloSwagging420 <yaoyixuan1992@gmail.com>
2021-05-01 15:32:26 -0400

    Add project files.

---
+++ StackLL/stackLLc++.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* next;
+};
+
+class stack
+{
+private:
+	Node* top;
+public:
+	stack() { top = NULL; }
+	void push(int x);
+	int pop();
+	void Display();
+};
+
+void stack::push(int x)
+{
+	Node* t = new Node;
+	if (t == NULL)
+		cout << "Stack is full\n";
+	else
+	{
+		t->data = x;
+		t->next = top;
+		top = t;
+	}
+}
+
+int stack::pop()
+{
+	int x = -1;
+	if (top == NULL)
+		cout << "Stack is empty\n";
+	else
+	{
+		x = top->data;
+		Node* t = top;
+		top = top->next;
+		delete t;
+	}
+	return x;
+}
+
+void stack::Display()
+{
+	Node* p = top;
+	while (p != NULL)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	stack stk;
+	stk.push(10);
+	stk.push(20);
+	stk.push(40);
+
+	stk.Display();
+	cout<< stk.pop();
+
+	return 0;
+}
